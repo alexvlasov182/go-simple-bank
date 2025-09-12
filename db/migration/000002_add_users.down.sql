84aab47fe01192af6b386317cd15729a8c7e4044
Oleksandr Vlasov <drumlife182@gmail.com>
2025-09-12 09:26:59 +0200

    fix: make migratedown 1

--- db/migration/000002_add_users.down.sql
+++ db/migration/000002_add_users.down.sql
@@ -1,9 +1,9 @@
 -- Drop the foreign key first
-ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "owner_currency_key";
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "fk_owner";
 
 -- Drop other constraints if needed
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "owner_currency_key";
 ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_key";
 
 -- Now drop dependent tables
-
 DROP TABLE IF EXISTS "users";
