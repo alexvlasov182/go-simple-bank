35889216c42d27ea77aac605882689c9c6336748
Oleksandr Vlasov <drumlife182@gmail.com>
2025-09-12 08:58:29 +0200

    docs: add users table with unique & foreign key constraints in PostgreSQL

---
+++ db/migration/000002_add_users.down.sql
@@ -0,0 +1,9 @@
+-- Drop the foreign key first
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "owner_currency_key";
+
+-- Drop other constraints if needed
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_key";
+
+-- Now drop dependent tables
+
+DROP TABLE IF EXISTS "users";
