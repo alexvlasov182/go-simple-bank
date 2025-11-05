ce01befe14dca1032e0785ba2a509778fb83f554
Oleksandr Vlasov <drumlife182@gmail.com>
2025-11-05 09:10:32 +0100

    load environment variable before running db migration

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ set -e
 /app/wait-for-it.sh $DB_HOST:5432 --timeout=30 --strict -- echo "Postgres is up"
 
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
